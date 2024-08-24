7a7b461e1ddae91d439fe3f39bc53611006a4124
SamN <162361529+Invokedzz@users.noreply.github.com>
2024-08-24 16:52:48 -0300

    Update matrix.ts

--- chapters/matrix.ts
+++ chapters/matrix.ts
@@ -30,7 +30,7 @@ export class receiveMatrix {
      //   private target: number,
     ) {};
 
-    startMatrix_analysis (target: number) {
+    startMatrix_analysis (target: number): number | boolean {
         let rows = this.matrix.length;
         let columns = this.matrix[0].length;
         for (let i = 0; i < rows; i++) {
@@ -59,5 +59,50 @@ const startClass = new receiveMatrix(
 
 startClass.startMatrix_analysis(target);
 
+export class matrixString {
+    constructor (
+        private matrixString: string [][],
+    ) {};
+
+    creatinglineofStrings (trg: string): boolean | string {
+        let row = this.matrixString.length;
+        let columnsString = this.matrixString[0].length;
+        for (let i = 0; i < row; i++) {
+            for (let j = 0; j < columnsString; j++) {
+                if (this.matrixString[i][j] === trg) {
+                    console.log(`You chose ${trg}`);
+                    return trg;
+                };
+            };
+        };
+
+        return false;
+
+    };
+
+}
+
+const createMatrixString = new matrixString(
+    [
+        ['Pablo Honey', 'Thom Yorke'],
+        ['Kurt Cobain', 'Amy Lee'],
+    ],
+);
+
+createMatrixString.creatinglineofStrings('Pablo Honey');
+
+const numberMatrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12],
+];
+
+console.log(numberMatrix);
+console.log(numberMatrix[0]);
+console.log(numberMatrix.length);
+console.log(numberMatrix[0].length);
+
+//console.log(numberMatrix[1].length);
 
 export default 1;
